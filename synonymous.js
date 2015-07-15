59f5cc574940e9f47288ca2310b1f9677cda412d
Alan Gutierrez <alan@prettyrobots.com>
2015-07-15 10:02:47 -0500

    Sketch of synonymous.

--- synonymous.js
+++ synonymous.js
@@ -1,1 +1,187 @@
-module.exports = 1
+var fs = require('fs'), slice = [].slice
+
+// The regular expression to match usage markdown.
+var USAGE_RE = /^\s*___(?:\s+(\w+)\s+_)?\s+(usage|strings)(?::\s+((?:[a-z]{2}_[A-Z]{2})(?:\s+[a-z]{2}_[A-Z]{2})*))?\s+___\s*/
+
+function extractUsage (source) {
+    var  numeric = /^(count|number|value|size)$/
+
+    var usage = _extractUsage(source)
+
+    usage.forEach(function (usage) {
+        usage.pattern = usage.pat = ''
+
+        // Extract a definition of the command line arguments from the usage message
+        // while tiding the usage message; removing special characters that are flags
+        // to Arguable that do not belong in the usage message printed to `stdout`.
+        usage.usage = usage.usage.map(function (line) {
+            var verbose, terse = '-\t', type = '!', arrayed, out = '', $, trim = /^$/
+            if ($ = /^(?:[\s*@]*(-[\w\d])[@\s]*,)?[@\s]*(--\w[-\w\d_]*)(?:[\s@]*[\[<]([^\]>]+)[\]>][\s@]*)?/.exec(line)) {
+                out = $[0], terse = $[1] || '-\t'
+                          , verbose = $[2]
+                          , type = $[3] && (numeric.test($[3]) ? '#' : '$') || '!'
+                          , line = line.substring(out.length)
+                arrayed = ~out.indexOf('@') ? '@' : ':'
+                usage.pattern = usage.pat += terse + ',' + verbose + arrayed + type + '|'
+                if (!line.length) trim = /\s+$/
+            }
+            return (out.replace('@', ' ') + line).replace(trim, '')
+        }).join('\n')
+    })
+
+    var object = {
+        usage: usage,
+        commands: (function () {
+            var commands = {}
+            usage.forEach(function (usage) {
+                if (usage.command) {
+                    commands[usage.command] = true
+                }
+            })
+            return commands
+        })(),
+        chooseUsage: function () {
+            var vargs = slice.call(arguments), command = vargs.shift()
+            var usages = []
+            if (command) {
+                usages.push.apply(usages, this.usage.filter(function (usage) {
+                    return command == usage.command && ~vargs.indexOf(usage.lang)
+                }.bind(this)))
+            } else {
+                usages.push.apply(usages, this.usage.filter(function (usage) {
+                    return ! usage.command && ~vargs.indexOf(usage.lang)
+                }))
+            }
+            if (!usages.length) {
+                if (vargs.length == 1 && vargs[0] == this.usage[0].lang) {
+                    return null
+                } else {
+                    return this.chooseUsage(command, this.usage[0].lang)
+                }
+            }
+            return usages.shift()
+        },
+        chooseString: function () {
+            var vargs = slice.call(arguments), command = vargs.shift(), key = vargs.pop()
+            var usages = []
+            if (command) {
+                usages.push.apply(usages, this.usage.filter(function (usage) {
+                    return command == usage.command && ~vargs.indexOf(usage.lang)
+                }.bind(this)))
+            }
+            usages.push.apply(usages, this.usage.filter(function (usage) {
+                return ! usage.command && ~vargs.indexOf(usage.lang)
+            }))
+            var chosen = usages.filter(function (usage) {
+                return usage.strings[key]
+            }).shift()
+            if (!chosen) {
+                if (vargs.length == 1 && vargs[0] == this.usage[0].lang) {
+                    return null
+                } else {
+                    return this.chooseString(command, this.usage[0].lang, key)
+                }
+            } else {
+                return chosen.strings[key]
+            }
+        }
+    }
+
+    return object
+}
+
+
+// Extract a usage message from a file.
+function extractUsage (source) {
+    var lines = fs.readFileSync(source, 'utf8').split(/\r?\n/),
+        i, j, I, line, indent, $, candidate, _default, usage,
+        message, command, match, langs,
+        usages = []
+
+    // **TODO**: Note that the test to see if we matched a line is if there is no
+    // sub-command specified, or else the sub-command matches the first argument.
+    // This would seem to imply that the default usage string should come last,
+    // but I've not written an application that has a default usage string yet.
+    //
+    // Need to allow the no sub-command to match, but then get vetoed by a matched
+    // sub-command.
+    //
+    // Note that the regular expression to match a usage line matches both the
+    // beginning and ending markup, so we need to check in our outer loop that
+    // we've matched beginning markup, and not ending markup, which would be an
+    // odd occurrence, probably worth abenending about.
+
+    //
+    OUTER: for (i = 0, I = lines.length;i < I; i++) {
+        if ($ = USAGE_RE.exec(lines[i])) {
+            if (!$[3]) continue OUTER;  // Matched ending markup.
+            langs = $[3].split(/\s+/)
+            command = $[1]
+            indent = /^(\s*)/.exec(lines[i])[1].length
+            for (j = i + 1; j < I; j++) {
+                if (/\S/.test(lines[j])) {
+                    indent = Math.min(indent, /^(\s*)/.exec(lines[j])[1].length)
+                }
+                if ($ = USAGE_RE.exec(lines[j])) {
+                    if (!message) {
+                        message = lines.slice(i + 1, j).map(function (line) { return line.substring(indent) })
+                        usage = { message: message }
+                        usage.command = command || null
+                    } else {
+                        usage.strings = strings(lines.slice(i + 1, j))
+                    }
+                    if ($[2] == 'strings') {
+                        i = j
+                        continue
+                    }
+                    langs.forEach(function (lang) {
+                        usage = {
+                            lang: lang,
+                            command: usage.command,
+                            usage: usage.message,
+                            strings: usage.strings || {}
+                        }
+                        if (!usage.command) delete usage.command
+                        usages.push(usage)
+                    })
+                    i = j - 1
+                    message = null
+                    continue OUTER
+                }
+            }
+            return []
+        }
+    }
+
+    return usages
+}
+
+// Extract message strings from the strings section of a usage message.
+function strings (lines) {
+    var i, I, j, J, $, spaces, key, order, line, message = [], dedent = Number.MAX_VALUE, strings = {}
+
+    OUTER: for (i = 0, I = lines.length; i < I; i++) {
+        if (($ = /^(\s*)([^:(]+)(?:\((\d+(?:\s*,\s*\d+)*)\))?:\s*(.*)$/.exec(lines[i]))) {
+            spaces = $[1].length, key = $[2].trim(), order = $[3] || '1', line = $[4], message = []
+            if (line.length) message.push(line)
+            for (i++; i < I; i++) {
+                if (/\S/.test(lines[i])) {
+                    $ = /^(\s*)(.*)$/.exec(lines[i])
+                    if ($[1].length <= spaces) break
+                    dedent = Math.min($[1].length, dedent)
+                }
+                message.push(lines[i])
+            }
+            for (j = line.length ? 1 : 0, J = message.length; j < J; j++) {
+                message[j] = message[j].substring(dedent)
+            }
+            if (message[message.length - 1] == '') message.pop()
+            strings[key] = { text: message.join('\n'), order: order.split(/\s*,\s*/) }
+            i--
+        }
+    }
+
+    return strings
+}
+
+module.exports = extractUsage
--- t/synonymous/synonymous.t.js
+++ t/synonymous/synonymous.t.js
@@ -1,5 +1,71 @@
+/*
+  ___ usage: en_US ___
+  usage: basic [options] [files]
+    -c, --config <key=value> @
+        --longonly
+
+  ___ usage ___
+*/
+
 require('proof')(1, prove)
 
 function prove (assert) {
-    assert(require('../..'), 'require')
+    var fs = require('fs'),
+        path = require('path'),
+        extractUsage = require('../..'),
+        options,
+        message = 'usage: basic [options] [files]\n' +
+                  '  -c, --config <key=value>\n' +
+                  '      --longonly\n' +
+                  ''
+
+    var usage = extractUsage(__filename)
+    var extracted = usage.usage
+    console.log(extracted)
+    assert(true, 'restart')
+    return
+    assert(extracted[0].pattern, '-c,--config@$|-\t,--longonly:!|', 'extracted pattern')
+    assert(extracted[0].usage, message, 'extracted message')
+    assert(usage.chooseUsage(null, 'xx_XX').usage, message, 'extracted message')
+
+    var usage = extractUsage(path.join(__dirname, 'sub.js'), [ 'run' ])
+    var sub = usage.usage
+    assert(sub[0].pattern, '-h,--help:!|-p,--processes:#|', 'extracted sub pattern')
+    assert(sub[0].usage, fs.readFileSync(path.join(__dirname, 'sub.txt'), 'utf8'), 'extracted sub message')
+    assert(sub[0].command, 'run', 'sub command')
+    assert(usage.chooseString('compile', 'en_US', 'example'),
+        { text: 'This is an example.', order: [ 1 ] }, 'choose string for particular command')
+
+    var i18n = extractUsage(path.join(__dirname, 'i18n.js')).usage
+    assert(i18n[2].usage, 'käyttö: awaken\n\n  Hyvää huomenta!', 'i18n Finnish')
+    assert(i18n[1].usage, 'uso: awaken\n\n  Buenos días!\n\nopciones:', 'i18n Spanish')
+
+    var usage = extractUsage(path.join(__dirname, 'strings.js'))
+    var strings = usage.usage
+    assert(strings[1].strings['main message'], {
+        text: 'This is the main message: %s.',
+        order: [ 1 ]
+    }, 'strings')
+    assert(strings[1].strings['immediate'], {
+        text: 'No space before or after.',
+        order: [ 1 ]
+    }, 'strings')
+    assert(strings[1].strings['following'], {
+        text: 'Message follows label.',
+        order: [ 1 ]
+    }, 'strings')
+    assert(strings[1].strings['multi line'], {
+        text: 'One line.\n\nAnd then another.',
+        order: [ 1 ]
+    }, 'strings')
+    assert(usage.chooseString(null, 'xx_XX', 'main message'), {
+        text: 'This is the main message: %s.',
+        order: [ 1 ]
+    }, 'default string')
+
+    var none = extractUsage(path.join(__dirname, 'missing.js')).usage
+    assert(none, [], 'missing')
+
+    var none = extractUsage(path.join(__dirname, 'endless.js')).usage
+    assert(none, [], 'endless')
 }
